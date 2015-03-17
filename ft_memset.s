39e5887a63c1bcd2edc5ea5755e3498bdb31c96a
Rida BIKITARAN <rbikitar@e3r5p20.42.fr>
2015-03-17 18:57:48 +0100

    memset

--- ft_memset.s
+++ ft_memset.s
@@ -2,42 +2,15 @@ section .text
 	global _ft_memset
 	
 _ft_memset:
-	mov r11, rdi
-	cmp r11, 0
+	cmp rdi, 0
 	je ret0
-	push rdi
-	mov rcx, 0
-	jmp length
-
-ft_memset2:	
-	pop rdi
-	mov r9, rcx
-	mov rcx, 0
-	jmp loop2
-
-length:
-	cmp byte [rdi + rcx], 0
-	je ft_memset2
-	inc rcx
-	jmp length
+	mov rcx, rdx
+	mov rax, rsi
+	mov rdx, rdi
+	cld
+	rep stosb
+	jmp rets
 	
-loop2:
-	cmp rcx, r9
-	jae rets
-	cmp rcx, rdx
-	jae end
-	mov [rdi + rcx], rsi
-	inc rcx
-	jmp loop2
-
-end:
-	inc rcx
-	jmp loop2
-	
-ret0:
-	mov rax, 0
-	ret
-
 rets:
-	mov rax, rdi
+	mov rax, rdx
 	ret
